bc84de323ba7ce27c8279cab3a290cfc5ebe54ec
AmineBAALI <amine.baali.perso@gmail.com>
2018-09-26 18:02:04 +0200

    client

---
+++ sources/jalon1/src/client/main.c
@@ -0,0 +1,67 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+#define Addr "127.0.0.1"
+#define Port "6666"
+#define len 100
+
+int main (int argc, char ** argv){
+
+  char message[len];
+  char recu[len];
+
+
+  int sock = socket( AF_INET, SOCK_STREAM, 0 );
+  struct sockaddr_in* sock_host = malloc (sizeof (struct sockaddr_in));
+  memset(sock_host, '\0', sizeof (struct sockaddr_in));
+  if (sock == -1){
+    printf ("erreur socket");
+    perror ("socket");
+  }
+
+  sock_host->sin_family = AF_INET;
+  sock_host->sin_port = htons(atoi(Port));
+  sock_host->sin_addr.s_addr = htonl(atoi(Addr));
+
+
+  int  connexion = connect (sock, (struct sockaddr*) sock_host, sizeof( struct sockaddr_in) );
+  if (connexion == -1){
+    printf ("erreur de connexion\n");
+    perror("co");
+  }
+
+  while(1){
+
+    for (int i=0; i<100; i++){
+      *(message+i)='0';
+      *(recu+i)='0';
+    }
+
+    printf("saisissez votre message (max 100 caractères)\n");
+    fgets (message, len, stdin);
+
+
+
+
+    int len_sent = send ( sock, message, len, 0);
+
+    int len_rcv= recv ( sock, recu, len, 0);
+
+    printf ("serveur :\n");
+    for (int j=0; j<len_rcv; j++){
+      printf ("%c", *(recu+j));
+    }
+    printf ("\n");
+
+
+
+  }
+  return 1;
+}
--- sources/jalon1/src/serveur/main.c
+++ sources/jalon1/src/serveur/main.c
@@ -10,28 +10,36 @@
 
 #define Addr "127.0.0.1"
 #define Port 6666
+#define len 100
 
 
 
 int main (int argc, char ** argv){
+
   int sock = socket( AF_INET, SOCK_STREAM, 0 );
+
   struct sockaddr_in* mon_adresse = malloc (sizeof (struct sockaddr_in));
+  memset(mon_adresse, '\0', sizeof(struct sockaddr_in));
   mon_adresse->sin_family = AF_INET;
-  mon_adresse->sin_port = Port;
-  mon_adresse->sin_addr.s_addr = atoi(Addr);
-  for (int i=0; i<8; i++){
-    *(i+(mon_adresse->sin_zero)) = 0;
-  }
+  mon_adresse->sin_port = htons(Port);
+  mon_adresse->sin_addr.s_addr = htonl(atoi(Addr));
+
   int e = bind (sock, (struct sockaddr*)mon_adresse, sizeof(struct sockaddr_in));
   struct sockaddr addr;
   socklen_t addrlen;
   while (1){
     int e2= listen (sock, 1);
     int e3= accept (sock, &addr, &addrlen);
-    char buf[25];
-    ssize_t len = 100;
+    char buf[len];
+
 
     ssize_t size =  recv(sock, &buf, len ,0);
+
+    printf ("client :\n");
+    for (int j=0; j<size; j++){
+      printf ("%c", *(buf+j));
+    }
+    printf ("\n");
 
     int e4 = send (sock, &buf, len, 0);
 
