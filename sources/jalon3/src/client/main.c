9b68522f6ab580903b1b206532d7a08843790662
Bruno <bbesse002@enseirb-matmeca.fr>
2018-10-12 00:59:12 +0200

    jalon3

--- sources/jalon3/src/client/main.c
+++ sources/jalon3/src/client/main.c
@@ -9,16 +9,52 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <poll.h>
-
-
+#include <pthread.h>
+#include <fcntl.h>
 
 
 #define len 100
 
+struct thread_arg{
+  char pseudo[22];
+  char pseudo_envoi[22];
+  int sock;
+  volatile int b;
+
+};
+
+void *envoi(struct thread_arg *arg){
+  char message[len];
+  *(arg->pseudo+strlen(arg->pseudo)-1)='\0';
+  *(arg->pseudo_envoi+strlen(arg->pseudo_envoi)-1)='\0';
+  while(arg->b){
+
+
+    strcpy(message,"");
+
+    printf("message de %s pour %s (max 100 caractères)\n",arg->pseudo, arg->pseudo_envoi);
+    fflush(stdout);
+    fgets (message, len, stdin);
+
+    int l = strlen(message);
+
+
+
+    int len_sent = send ( arg->sock,message, l+1, 0);
+  }
+  free(arg);
+}
+
+
+
+
 
 int main (int argc, char ** argv){
 
-  char q[]="/quit";
+  char  q[]="/quit\n";
+  char w[]="/who\n";
+  char wi[]="/wois";
+  char n[]="/nick";
 
   if (argc!=3){
     printf ("arguments");
@@ -29,9 +65,16 @@ int main (int argc, char ** argv){
   int Port = atoi(argv[2]);
 
 
-  char message[len];
+
   char recu[len];
+  char pseudo[22];
+  char liste[500];
   char test_b[] = "0\n";
+  char pseudo_envoi[22];
+  char co_part[500];
+  char t[]="0";
+  int q1=0;
+  int flags;
 
   int sock = socket( AF_INET, SOCK_STREAM, 0 );
   struct sockaddr_in sock_host;
@@ -42,6 +85,9 @@ int main (int argc, char ** argv){
     return 0;
   }
 
+  struct pollfd fds[1];
+    fds[0].fd = sock;
+    fds[0].events = POLLIN;
 
 
 
@@ -52,7 +98,10 @@ int main (int argc, char ** argv){
   sock_host.sin_addr.s_addr = inet_addr(IP);
 
 
+
+
   int connexion = connect(sock,(struct sockaddr*)&sock_host,sizeof(sock_host));
+
 
   if (connexion == -1){
     perror("erreur de connexion\n");
@@ -62,50 +111,115 @@ int main (int argc, char ** argv){
 
 
 
-    recv(sock, test_b, 3, 0);
+  recv(sock, test_b, 2, 0);
 
 
-  char t[]="0\n";
+
   if (strcmp(test_b,t) == 0){
-    printf("connexion refusée par le serveur\n");
+    printf("connexion refusee par le serveur\n");
     fflush(stdout);
     return 0;
   }
 
 
 
+
+  strcpy(liste," ");
+  strcpy(co_part," ");
+
+  printf ("choisissez un pseudo (max. 20 car.):\n");
+  fgets (pseudo, len, stdin);
+  int l1 = strlen(pseudo);
+  send ( sock, pseudo, l1+1, 0);
+  recv ( sock, liste, 500, 0);
+  while (strcmp(liste,"/client_non_cree")==0){
+    printf ("pseudo invalide\n");
+    fgets (pseudo, len, stdin);
+    l1 = strlen(pseudo);
+    send ( sock, pseudo, l1+1, 0);
+    recv ( sock, liste, 500, 0);
+  }
+
   while (1){
 
-    for (int i=0; i<100; i++){
-      *(message+i)='0';
-      *(recu+i)='0';
-    }
-
-    printf("saisissez votre message (max 100 caractères)\n");
-    fgets (message, len, stdin);
-
-    int l = strlen(message);
-
-
-
-    int len_sent = send ( sock, message, l, 0);
-
-
-    int len_rcv= recv ( sock, recu, len, 0);
-
-    if (strncmp(recu, q,5)==0){
-      printf ("connexion terminée\n");
-      return 0;
-    }
-    else{
-      printf ("serveur :\n");
-      for (int j=0; j<len_rcv; j++){
-        printf ("%c", *(recu+j));
+    printf ("%s\n",liste);
+    fgets (pseudo_envoi, len, stdin);
+    int l2 = strlen(pseudo_envoi);
+    send ( sock, pseudo_envoi, l2+1, 0);
+    q1= recv ( sock, co_part, 500, 0);
+    while (strstr(co_part,"/communication_etablie")==NULL){
+      printf ("%s\n",co_part);
+      poll(fds,1,1);
+      while (fds[0].revents==POLLIN){
+        recv ( sock, &co_part, 500, 0);
+        printf ("%s\n",co_part);
+        fflush(stdout);
+        poll(fds,1,1);
       }
-      printf ("\n");
+      fflush(stdout);
+      fflush(stdin);
+      fgets (pseudo_envoi, len, stdin);
+      fflush(stdin);
+      l2 = strlen(pseudo_envoi);
+      send ( sock, pseudo_envoi, l2+1, 0);
+      recv ( sock, co_part, 500, 0);
     }
+    printf ("%s\n",co_part);
+    fflush(stdout);
+      struct thread_arg *arg=malloc (sizeof(struct thread_arg));
+      strcpy(arg->pseudo,pseudo);
+      strcpy(arg->pseudo_envoi,pseudo_envoi);
+      arg->sock=sock;
+      arg->b=1;
+
+    pthread_t th;
+    pthread_create(&th, NULL, (void *)envoi,arg);
+
+    while (1){
+
+      strcpy(recu,"");
 
 
+      int len_rcv= recv ( sock, recu, len, 0);
+
+      if (strstr(recu, q)!=NULL){
+        printf ("connexion terminée\n");
+        fflush(stdout);
+        void* ret;
+        arg->b=0;
+        pthread_exit( ret);
+        return 0;
+      }
+      else if (strstr(recu, n)!=NULL){
+        void* ret;
+        arg->b=0;
+        pthread_exit( ret);
+        strcpy(liste," ");
+        strcpy(co_part," ");
+        strcpy(pseudo,(recu+6));
+        break;
+      }
+      else if (strstr(recu,"personne de connecte pour le moment (envoyez un message pour rafaichir)\n")!=NULL){
+        void* ret;
+        arg->b=0;
+        pthread_exit( ret);
+        strcpy(liste," ");
+        strcpy(co_part," ");
+        break;
+      }
+      else if (strncmp(recu,w,4)==0){
+        void* ret;
+        arg->b=0;
+        pthread_exit( ret);
+        strcpy(liste," ");
+        strcpy(co_part," ");
+        break;
+      }
+      else{
+        printf ("%s\n", recu);
+      }
+
+    }
 
   }
   return 1;
--- sources/jalon3/src/serveur/main.c
+++ sources/jalon3/src/serveur/main.c
@@ -9,19 +9,24 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <poll.h>
-
-
-struct client{
-  int i;
-  char pseudo[21];
-  struct client* com;
-  struct client* next;
-
-}
+#include <fcntl.h>
 
 #define len 100
 
+struct client{
+  int i;
+  char pseudo[22];
+  struct client* com;
+  struct client* next;
+
+};
+
+
+
 struct client* trouver_client(struct client* client0, char* pseudo){
+  if (strlen(pseudo)<1){
+
+  }
   struct client* client_actuel = client0;
   while (1){
     if (client_actuel == NULL){
@@ -36,10 +41,31 @@ struct client* trouver_client(struct client* client0, char* pseudo){
   }
 }
 
+struct client* trouver_client_indice(struct client* client0, int i){
+  struct client* client_actuel = client0;
+  while (1){
+    if (client_actuel == NULL){
+      return NULL;
+    }
+    else if (client_actuel->i == i){
+      return client_actuel;
+    }
+    else {
+      client_actuel = client_actuel->next;
+    }
+  }
+}
+
 
 int creer_client (struct client* client0, int i, char* pseudo){
   if (trouver_client(client0, pseudo) != NULL){
     return -1;
+  }
+  struct client* a =trouver_client_indice(client0,i);
+  if (a!=NULL){
+    strcpy(a->pseudo,pseudo);
+    a->com=NULL;
+    return 1;
   }
   else{
     struct client* client_actuel = client0;
@@ -51,26 +77,65 @@ int creer_client (struct client* client0, int i, char* pseudo){
     client_actuel->next =NULL;
     client_actuel->i=i;
     client_actuel->com = NULL;
-    client_actuel->pseudo = pseudo;
+    strcpy(client_actuel->pseudo,pseudo);
     return 1;
   }
 }
 
+int supprimer_client (struct client* client0, int i){
+  struct client *client_actuel =client0;
+  if (client_actuel->next==NULL){
+    return -1;
+  }
+  while (client_actuel->next->i!=i && client_actuel->next->next!=NULL){
+    client_actuel=client_actuel->next;
+  }
+  if (client_actuel->next->i == i){
+    struct client* ptr = client_actuel->next;
+    client_actuel->next=client_actuel->next->next;
+    client_actuel=client0;
+    while (client_actuel != NULL){
+      if (client_actuel->com == ptr) client_actuel->com =NULL;
+      client_actuel=client_actuel->next;
+    }
+    free(ptr);
+    return 1;
+  }
+  else return 0;
+}
 
+void liste_utilisateurs(struct client* client0,int i, char tmp[500]){
+  struct client * client_actuel= client0->next;
+  strcpy(tmp, "choisissez un contact :\n");
+  if (client_actuel->next==NULL){
+    strcpy(tmp,"personne de connecte pour le moment (envoyez un message pour rafaichir)\n");
+    exit;
+  }
+  while (client_actuel != NULL){
+    if (client_actuel->i !=i){
+      strcat(tmp,client_actuel->pseudo);
+    }
+    client_actuel=client_actuel->next;
+  }
+}
 
 
 int main (int argc, char ** argv){
   struct client* client0=malloc(sizeof(struct client));
 
   client0->next=NULL;
-  client0->pseudo="server";
+  strcpy(client0->pseudo,"server");
   client0->i=-1;
   client0->com = NULL;
 
 
   int co_max =20;
 
-  char  q[7]="/quit\n";
+  char  q[]="/quit\n";
+  char w[]="/who\n";
+  char wi[]="/wois\n";
+  char n[]="/nick";
+
 
   if (argc!=3){
     printf ("arguments");
@@ -117,14 +182,14 @@ int main (int argc, char ** argv){
 
           if (k<co_max){
             fds[i].fd=accept(sock, &addr, &addrlen);
-            int ls = send(fds[i].fd, "1\n",3,0);
+            int ls = send(fds[i].fd, "1",2,0);
             fds[i].events=POLLIN;
             k++;
             break;
           }
           else{
             int temp = accept(sock, &addr, &addrlen);
-            int ls = send(temp, "0\n", 3, 0);
+            int ls = send(temp, "0", 2, 0);
             close(temp);
             break;
           }
@@ -136,25 +201,83 @@ int main (int argc, char ** argv){
 
           char buf[len];
 
-          ssize_t size =  recv(fds[i].fd, &buf, len ,0);
+          ssize_t size =  recv(fds[i].fd, buf, len ,0);
 
-          printf ("client :\n");
+          printf ("%d :\n",fds[i].fd);
           for (int j=0; j<size; j++){
             printf ("%c", *(buf+j));
           }
           printf ("\n");
-          int e4 = send (fds[i].fd, &buf, size, 0);
-
-          if (strncmp(buf,q,7)==0){
+          fflush(stdout);
+          if (strcmp(buf,q)==0){
+            int e4 = send (fds[i].fd, buf, size, 0);
             printf ("connexion avec un client fermée\n");
             fflush (stdout);
+            supprimer_client(client0,i);
             close (fds[i].fd);
             k--;
             fds[i].fd = sock;
             fds[i].events = POLLIN;
             break;
           }
+          else if (strncmp(buf,n,5)==0){
+            if (creer_client(client0,i,buf+6)==1){
+              char tmp[500];
+              liste_utilisateurs(client0,i,tmp);
+              send (fds[i].fd, tmp, 500, 0);
+            }
+            else{
+              send (fds[i].fd, "entrez un pseudo valable", 17, 0);
+            }
+          }
+          else if (strcmp(buf,w)==0){
+            struct client* az = trouver_client_indice(client0,i);
+            az->com=NULL;
+            send (fds[i].fd, buf, 500, 0);
+          }
+          else if (strcmp(buf,wi)==0){
 
+          }
+          else{
+            if (trouver_client_indice(client0,i)==NULL){
+              if (creer_client(client0,i,buf)==1){
+                char tmp [500];
+                liste_utilisateurs(client0,i,tmp);
+                send (fds[i].fd, tmp, 500, 0);
+              }
+              else{
+                send (fds[i].fd, "/client_non_cree", 17, 0);
+              }
+              break;
+            }
+            else {
+              struct client* ptr = trouver_client_indice(client0,i);
+              if (ptr->com ==NULL){
+                struct client* correspondant = trouver_client(client0,buf);
+                if (correspondant !=NULL){
+                  ptr->com=correspondant;
+                  send (fds[i].fd, "/communication_etablie", 23, 0);
+                }
+                else{
+                  char tmp [500];
+                  liste_utilisateurs(client0,i,tmp);
+                  send (fds[i].fd, tmp, 500, 0);
+                }
+              }
+              else{
+                char msg[size+22];
+                strcpy(msg,ptr->pseudo);
+                msg[strlen(ptr->pseudo)]=' ';
+                msg[strlen(ptr->pseudo)-1]='\0';
+                strcat(msg," :");
+                strcat(msg,buf);
+                send (fds[ptr->com->i].fd, &msg, size+22, 0);
+
+              }
+              break;
+            }
+
+          }
 
         }
       }
